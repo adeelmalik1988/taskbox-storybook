f1fbc925064700071f2c1b91c4d9076c46085b4f
Adeel Malik <adeelmalik1988@gmail.com>
2020-11-25 20:25:26 +0500

    finalizing for publishing

--- src/components/InboxScreen/InboxScreen.stories.tsx
+++ src/components/InboxScreen/InboxScreen.stories.tsx
@@ -5,25 +5,55 @@ import * as TaskListStories from '../TaskList/TaskList.stories';
 import { action } from '@storybook/addon-actions'
 import { Provider } from 'react-redux';
 import TaskStories from '../Task/Task.stories';
-//import store from '../../lib/store'
+import store from '../../lib/store'
+import { AnyAction, EnhancedStore, Store } from '@reduxjs/toolkit';
+//import { AnyAction, } from 'redux';
+import { TaskListProps } from '../TaskList/TaskList';
+import configureStore from 'redux-mock-store' 
+//import configureStore from 'redux-mock-store/dist/index-umd'
+import { act } from 'react-dom/test-utils';
 
 
-const store = {
+//const store: EnhancedStore<TaskListProps, AnyAction, [ThunkMiddleware<TaskListProps, AnyAction, null> | ThunkMiddleware<TaskListProps, AnyAction, undefined>]>
+/*
+
+const store: Store<any, AnyAction >
+    = {
     getState: () => {
         return {
             tasks: TaskListStories.Default.args?.tasks,
         }
     },
+    dispatch: () => action('dispatch'),
     subscribe: () => 0,
-    dispatch: action('dispatch')
 }
+*/
+
+/////Mock Store/////
+
+//const { configureStore } = require('redux-mock-store') //CommonJS
+//const configureMockStore = require('redux-mock-store');
+/*
+const middlewares: any = []
+const mockStore = configureStore(middlewares)
+
+const initialState: TaskListProps = { tasks: TaskListStories.Default.args?.tasks}
+const store = mockStore({
+    initialState,
+    getState: () => {return initialState},
+    dispatch: action('dispatch'),
+    subscribe: () => 0,
+    
+}) 
+*/
+//const actionFunction= ()=> action('dispatch')
 
 
 export default {
     component: PureInboxScreen,
     title: 'InboxScreen',
     decorators: [story => <Provider store={store} >
-        { story() }
+        {story()}
     </Provider>]
 } as Meta
 
